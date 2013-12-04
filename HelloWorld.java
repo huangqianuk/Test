b0b77767160c260372c3f6a746851777832a849d
huangqianuk <huangqianuk@gmail.com>
2013-12-05 03:00:38 +0800

    HelloWorld.jaba

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Out Print "Hello World!" 
+ * @author HuangQian
+ *
+ */
+public class HelloWorld {
+
+	public static void main ( String [] args ) {
+		System.out.println( "Hello World!");
+	}
+}
